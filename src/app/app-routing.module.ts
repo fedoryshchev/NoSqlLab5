34cb157f442caaa2e05a05bd041c92f10fd1fc16
Dmitriy Fedoryshchev <Dmitriy.Fedoryshchev@ibm.com>
2019-05-23 12:11:49 +0300

    add routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+import { NotesComponent } from './components/notes/notes.component';
+import { NoteComponent } from './components/note/note.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
+
+const routes: Routes = [
+  {
+    path: "signin",
+    component: SignInComponent
+  },
+  {
+    path: "signup",
+    component: SignUpComponent
+  },
+
+  {
+    path: "notes",
+    component: NotesComponent
+  },
+  {
+    path: "note/create",
+    component: EditNoteComponent
+  },
+  {
+    path: "note/edit/:id",
+    component: EditNoteComponent
+  },
+  {
+    path: "note/:id",
+    component: NoteComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
